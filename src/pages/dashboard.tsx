1d84ea5926a6ec035fb9ecd9fabd67c87eee11be
Ivan Wong Hong Zheng <111694151+ivanwong1223@users.noreply.github.com>
2025-02-27 16:20:45 +0800

    environment_structure-setup

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const dashboard = () => {
+  return (
+    <div>dashboard HELLO WORLD JIBAI</div>
+  )
+}
+
+export default dashboard
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,15 @@
 import Image from "next/image";
+import '../app/globals.css';
+import '../app/favicon.ico';
+import Header from "../components/reusable/Header";
 
 export default function Home() {
+
+  console.log("This is to call API_KEY from env: ", process.env.NEXT_PUBLIC_PINATA_API_KEY);
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      {/* <Header /> This is header section */}
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
@@ -16,16 +23,15 @@ export default function Home() {
           <li className="mb-2">
             Get started by editing{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
+              src/pages/index.tsx
             </code>
             .
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
-
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-black text-white gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
